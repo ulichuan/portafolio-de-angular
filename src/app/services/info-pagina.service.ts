19144992e4143d601c33085a2d8dde9abca4237f
ulichuan <pigmento@me.com>
2019-08-11 23:35:19 +0100

    find del video 33 con Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,39 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[];
 
   constructor( private http: HttpClient ) { 
 
     // console.log('Servicio de infoPagina listo');
 
+    // vamos rescatando los datos del private
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON y tomar sus propiedades para usar en las pagnas
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
-
       });
   }
+
+  private cargarEquipo() {
+    // Leer el archivo JSON y tomar sus propiedades para usar en las pagnas
+    this.http.get('https://angular-html-dcccc.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo = resp;
+        console.log(resp);
+      });
+
+    // this.equipo = resp
+  }
+
+
 }
