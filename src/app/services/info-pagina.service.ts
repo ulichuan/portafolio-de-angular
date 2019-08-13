938eafe8b5c475df9fa8f50f7388e72b9c8ff4f4
ulichuan <pigmento@me.com>
2019-08-13 22:46:12 +0100

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -38,7 +38,7 @@ export class InfoPaginaService {
       .subscribe( (resp: any[]) => {
 
         this.equipo = resp;
-        console.log(resp);
+        // console.log(resp);
       });
 
     // this.equipo = resp
