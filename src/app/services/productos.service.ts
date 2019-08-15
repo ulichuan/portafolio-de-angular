6e3db6c388ab594b0e6bf5c2703402da04ce1a05
ulichuan <pigmento@me.com>
2019-08-15 14:33:28 +0100

    Creando la logica de las busquedas

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
 
@@ -18,19 +21,41 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-dcccc.firebaseio.com/productos_idx.json')
+    return new Promise( ( resolve, reject ) => {
+      this.http.get('https://angular-html-dcccc.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[]) => {
-
         this.productos = resp;
-        setTimeout(() => {
-          this.cargando = false;
-        }, 2000);
-      });
+        this.cargando = false;
+        resolve();
+        });
+    });
+
   }
 
   getProducto( id: string ) {
     return this.http.get(`https://angular-html-dcccc.firebaseio.com/productos/${ id }.json`);
   }
 
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar despues de tener los productos
+        // aquí aplicar el filtro
+        this.filtrarProductos( termino ):
+      });
+
+    } else {
+      // apllicar el filtro
+    }
+
+
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+  }
 
 }
